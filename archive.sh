ffb48dc4c0480d4d0b84589d3f5c31e524ee8fc0
Bennet Klarhoelter <mail@bennetklarhoelter.de>
2024-07-20 07:45:51 +0200

    refactoring

--- archive.sh
+++ archive.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-rm -f BoehsermoeHbConnector.zip
-cd src
-zip -vr ../BoehsermoeHbConnector.zip . -x "*.DS_Store" "__MACOSX/*"
-cd ..
+#rm -f BoehsermoeHbConnector.zip
+#cd src
+#zip -vr ../BoehsermoeHbConnector.zip .
+#cd ..
 zip BoehsermoeHbConnector.zip -d "*.DS_Store" "__MACOSX/*"
 
 #open .
