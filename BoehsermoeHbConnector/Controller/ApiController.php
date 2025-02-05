cf0ca3c0075c1d58bff698513cd48bc4de13d3d4
Bennet Klarhoelter <mail@bennetklarhoelter.de>
2025-02-05 14:44:25 +0100

    shopware 6.6

--- BoehsermoeHbConnector/Controller/ApiController.php
+++ BoehsermoeHbConnector/Controller/ApiController.php
@@ -12,7 +12,7 @@ use Shopware\Core\Framework\Struct\ArrayEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use function md5;
 
 #[Route(defaults: ['_routeScope' => ['api']])]
